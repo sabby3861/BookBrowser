21e18e763bf78549f4e44325ce7fcaba9c06a917
Sanjay Chauhan <sabby3861@gmail.com>
2026-03-30 00:47:14 +0530

    Initial Commit

---
+++ BookBrowser/BookBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBrowserApp.swift
+//  BookBrowser
+//
+//  Created by Sanjay Kumar on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookBrowserTests/BookBrowserTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookBrowserTests.swift
+//  BookBrowserTests
+//
+//  Created by Sanjay Kumar on 30/03/2026.
+//
+
+import Testing
+
+struct BookBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
